d936867639e27aecd7c48aa2b425bb01e65de799
hoanghoa11 <hoanghoa11902@gmail.com>
2026-03-08 14:48:28 +0700

    connect with S3

--- .github/actions/deploy-s3-javascript/index.cjs
+++ .github/actions/deploy-s3-javascript/index.cjs
@@ -1,6 +1,15 @@
 const core = require('@actions/core');
-
+const { exec } = require('@actions/exec');
 function run() {
+  // Get inputs
+  const bucket = core.getInput('bucket-name', { required: true });
+  const region = core.getInput('region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+  
+  //Upload files to S3 (this is a placeholder, you would implement the actual upload logic here)
+  const s3Uri = `s3://${bucket}`;
+  exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`)
+  
   core.notice('Deploying to S3...');
 }
 
